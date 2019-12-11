b383982aaeee0dd1fb469c4c0b0de9d4bb676855
HossainAbedy <abedy.ewu@gmail.com>
2019-12-11 17:07:51 +0600

    updated 3rd

--- app/Http/Controllers/AbdullahController.php
+++ app/Http/Controllers/AbdullahController.php
@@ -18,7 +18,7 @@ class AbdullahController extends Controller
     public function search(Request $request) {
         $keyword = '';
         $date = '';
-        $group = '';
+        $groups = '';
 
         if(isset($request['search'])){
             $keyword = $request['search'];
@@ -26,18 +26,27 @@ class AbdullahController extends Controller
         if(isset($request['date_filter'])){
             $date = $request['date_filter'];
         }
+        
         if(isset($request['group_filter'])){
             $group = $request['group_filter'];
-            $groups = SocialPostGroups::where('type', '$group')->get();
         }
 
-        $posts = BufferPosting::where('post_text', 'like', '%'.$keyword.'%')
-                    ->where('sent_at', 'like', '%'.$date.'%')
-                    ->where('group_id', 'like', '%'.$data->id.'%')
-                    ->orderBy('id', 'desc')
-                    ->paginate(10);
+        // $posts = BufferPosting::where('post_text', 'like', '%'.$keyword.'%')
+        //             ->where('sent_at', 'like', '%'.$date.'%')
+        //             ->where('group_id', 'like', '%'.$groups->id.'%')
+        //             ->orderBy('id', 'desc')
+        //             ->paginate(10);
 
-        $groups = SocialPostGroups::get();
+        $posts = BufferPosting::where('post_text', 'like', '%'.$keyword.'%')
+                ->where('sent_at', 'like', '%'.$date.'%')
+                ->whereHas('groupInfo', function ($query) use ($group) {
+                    $query->where('type', 'like', "%{$group}%");
+                })
+                ->orderBy('id', 'desc')
+                ->paginate(10);
+        
+
+        $groups = SocialPostGroups::distinct()->get(['type']);
 
         return view('pages.abdullah')-> with(compact('posts', 'keyword', 'groups'));  
          
